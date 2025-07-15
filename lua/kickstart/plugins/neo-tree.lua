5cea9162eb628c4daccd188352aa543423b8eb79
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-07-15 20:13:05 +0200

    neotree watches files

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -15,6 +15,10 @@ return {
   },
   opts = {
     filesystem = {
+      follow_current_file = {
+        enabled = true,
+      },
+      use_libuv_file_watcher = true,
       filtered_items = {
         visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
         hide_dotfiles = false,
