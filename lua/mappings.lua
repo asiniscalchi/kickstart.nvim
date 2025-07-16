b04cd9bf94b08bbcb61335eab3921ac1e0bb9786
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-07-16 15:04:15 +0000

    remapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,11 +1,4 @@
-vim.keymap.set('n', '<leader>h', function()
-  local height = math.floor(vim.o.lines * 0.3)
-  vim.cmd(height .. 'split | terminal')
-  vim.cmd 'setlocal winfixwidth'
-  vim.cmd 'startinsert'
-end, { noremap = true, silent = true })
-
-vim.keymap.set('n', '<leader>v', function()
+vim.keymap.set('n', '<leader>a', function()
   local total_cols = vim.o.columns -- Total number of columns in the window
   local width = math.floor(total_cols * 0.25) -- 30% of the total width
   vim.cmd 'botright vsplit' -- Create the vertical split
@@ -13,4 +6,11 @@ vim.keymap.set('n', '<leader>v', function()
   vim.cmd 'setlocal winfixwidth'
   vim.cmd 'terminal aichat' -- run the executable
   vim.cmd 'startinsert' -- enter insert mode
-end, { noremap = true, silent = true })
+end, { noremap = true, silent = true, desc = 'Open aichat' })
+
+vim.keymap.set('n', '<leader>b', function()
+  local height = math.floor(vim.o.lines * 0.3)
+  vim.cmd(height .. 'split | terminal')
+  vim.cmd 'setlocal winfixwidth'
+  vim.cmd 'startinsert'
+end, { noremap = true, silent = true, desc = 'Open terminal' })
