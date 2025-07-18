1082b75e695f419c667b3242a8915ad40b28843f
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-07-18 09:20:55 +0200

    removed the ai keymap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,16 +1,5 @@
-vim.keymap.set('n', '<leader>a', function()
-  local total_cols = vim.o.columns -- Total number of columns in the window
-  local width = math.floor(total_cols * 0.25) -- 30% of the total width
-  vim.cmd 'botright vsplit' -- Create the vertical split
-  vim.cmd('vertical resize ' .. width) -- Resize to 30% of total width
-  vim.cmd 'setlocal winfixwidth'
-  vim.cmd 'terminal aichat' -- run the executable
-  vim.cmd 'startinsert' -- enter insert mode
-end, { noremap = true, silent = true, desc = 'Open aichat' })
-
 vim.keymap.set('n', '<leader>x', function()
   local height = math.floor(vim.o.lines * 0.3)
   vim.cmd(height .. 'split | terminal')
   vim.cmd 'setlocal winfixwidth'
-  vim.cmd 'startinsert'
 end, { noremap = true, silent = true, desc = 'Open terminal' })
